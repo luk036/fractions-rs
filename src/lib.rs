b965500ff98c2eea123b59816543787661eda7fa
Wai-Shing Luk <luk036@gmail.com>
2022-07-12 14:19:49 +0800

    add quickcheck

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,9 @@ pub use crate::fractions::Fraction;
 
 #[cfg(test)]
 mod tests {
-    use crate::fractions::Fraction;
     use num_integer::gcd;
+    use super::*;
+    use quickcheck_macros::quickcheck;
 
     #[test]
     fn it_works() {
@@ -115,5 +116,61 @@ mod tests {
         assert_eq!(infp - infp, nan);
         assert_eq!(infp - pos, infp);
         assert_eq!(infn + pos, infn);
+    }
+
+    #[quickcheck]
+    fn check_eq(num: u32, den: u32) -> bool {
+        let p = Fraction::new(num as i32, den as i32);
+        p == p
+    }
+
+    #[quickcheck]
+    fn check_neg(num: u32, den: u32) -> bool {
+        let p = Fraction::new(num as i32, den as i32);
+        p == -(-p)
+    }
+
+    // #[quickcheck]
+    // fn check_reciprocal(num: i32, den: i32) -> bool {
+    //     let mut p = Fraction::new(num/2 , den/2);
+    //     let q = p;
+    //     p.reciprocal();
+    //     p.reciprocal();
+    //     p == q
+    // }
+
+    #[quickcheck]
+    fn check_default(num: u32, den: u32) -> bool {
+        let p = Fraction::new(num as i32, den as i32);
+        let zero = Fraction::<i32>::default();
+        p == p + zero
+    }
+
+    #[quickcheck]
+    fn check_order(d1: u16, d2: u16) -> bool {
+        let p = Fraction::new(300000001, d1 as i32);
+        let q = Fraction::new(400000001, d2 as i32);
+        p < q || p > q || p == q
+    }
+
+    #[quickcheck]
+    fn check_mul(d1: u16, d2: u16) -> bool {
+        let p = Fraction::new(300000001_i64, d1 as i64);
+        let q = Fraction::new(400000001_i64, d2 as i64);
+        p * q == q * p
+    }
+
+    #[quickcheck]
+    fn check_add(d1: u16, d2: u16) -> bool {
+        let p = Fraction::new(300000001_i64, d1 as i64);
+        let q = Fraction::new(400000001_i64, d2 as i64);
+        p + q == q + p
+    }
+
+    #[quickcheck]
+    fn check_add_sub(n1: u16, n2: u16) -> bool {
+        let p = Fraction::new(n1 as i64, 300000001_i64);
+        let q = Fraction::new(n2 as i64, 400000001_i64);
+        p == (p + q) - q
     }
 }
